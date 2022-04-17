b15309499aa7d0966eeb8e17087af53ca25693ea
Bogdan <zykovbogdan07@gmail.com>
2022-04-17 21:04:04 +0300

    Initial Commit

---
+++ MapApp/MapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppApp.swift
+//  MapApp
+//
+//  Created by Богдан Зыков on 17.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct MapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
